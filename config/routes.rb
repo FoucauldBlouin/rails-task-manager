16f5f7f1d89a4fe60b8e5dff1de4abd93bbd77e7
foucauld blouin <foucauld.blouin@gmail.com>
2023-05-23 15:29:36 +0200

    1st step

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+  get 'tasks/new', to: 'tasks#new', as: 'new_task'
+  post 'tasks', to: 'tasks#forms'
+
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
---
+++ db/migrate/20230523095120_rename_detail_to_details.rb
@@ -0,0 +1,5 @@
+class RenameDetailToDetails < ActiveRecord::Migration[7.0]
+  def change
+    rename_column :tasks, :detail, :details
+  end
+end
